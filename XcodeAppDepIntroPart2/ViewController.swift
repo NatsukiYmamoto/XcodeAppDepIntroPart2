d1331fff9b4a9ed441d3dde9f8ed0ff7fae40e53
Yanmo_swift <dntangfrvr@gmail.com>
2020-12-10 22:41:50 +0900

    Initial Commit

---
+++ XcodeAppDepIntroPart2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XcodeAppDepIntroPart2
+//
+//  Created by 山本 夏紀 on 2020/12/10.
+//  Copyright © 2020 natsuki.yamamoto2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
